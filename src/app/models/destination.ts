728f3275dd7674e440b7ef89c5100c354492438b
clauga <guerrero.ayala.cs@gmail.com>
2024-02-15 16:03:16 +0100

    modif interface

--- src/app/models/destination.ts
+++ src/app/models/destination.ts
@@ -1,8 +1,24 @@
-export interface Destination {
+export class Destination {
   type: string;
   name: string;
   country: string;
   image: string;
   price: number;
   location: string;
+
+  constructor(
+    type: string,
+    name: string,
+    country: string,
+    image: string,
+    price: number,
+    location: string
+  ) {
+    this.type = type;
+    this.name = name;
+    this.country = country;
+    this.image = image;
+    this.price = price;
+    this.location = location;
+  }
 }
