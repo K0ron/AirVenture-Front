60dd91ea9016ac4123cd62c973960bd43320f3cf
clauga <guerrero.ayala.cs@gmail.com>
2024-02-15 11:27:37 +0100

    search-bar component

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-search-bar',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatIconModule,
+    FormsModule,
+    MatFormFieldModule,
+  ],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.css',
+})
+export class SearchBarComponent {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  enteredSearchValue: string = '';
+
+  @Output()
+  searchTextChanged: EventEmitter<string> = new EventEmitter<string>();
+
+  onSearchTextChange() {
+    this.searchTextChanged.emit(this.enteredSearchValue);
+  }
+}
