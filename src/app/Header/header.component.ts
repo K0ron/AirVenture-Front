3f60cbcaf191d3a883b527e19940a9d3e8e1f8ee
Thomas Geslin <thomas.geslin31@gmail.com>
2024-02-22 11:25:36 +0100

    feat: header update

--- src/app/Header/header.component.ts
+++ src/app/Header/header.component.ts
@@ -1,8 +1,10 @@
-import {Component, ElementRef, Renderer2} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Output, Renderer2} from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {NavigationEnd, Router, RouterLink} from "@angular/router";
+import {MatCardModule} from "@angular/material/card";
 
 @Component({
   selector: 'app-header',
@@ -12,19 +14,43 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatIconModule,
     MatButtonModule,
     MatToolbarModule,
+    MatCardModule,
+    RouterLink,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
-  ngOnInit() {
-    const header = this.elementRef.nativeElement.querySelector('#header');
+  constructor(private router: Router) {}
 
-    if(window.location.pathname === '/login') {
-      this.renderer.setStyle(header, 'display', 'none');
-    } else {
-      this.renderer.setStyle(header, 'display', 'block');
-    }
+  ngOnInit() {
+    const header = document.getElementById('header')!;
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if(window.location.pathname === '/login') {
+          header.setAttribute("style", "display: none");
+        } else {
+          header.setAttribute("style", "display: flex");
+        }
+      }
+    });
+  }
+
+  openNavigation() {
+    const target = document.getElementById('headerNav')!;
+    target.classList.add('onOpen');
+    const background = document.getElementById('backgroundNavigation')!;
+    background.classList.add('darkerBackground');
+  }
+
+  closeNavigation() {
+    const target = document.getElementById('headerNav')!;
+    target.classList.remove('onOpen');
+    const background = document.getElementById('backgroundNavigation')!;
+    background.classList.remove('darkerBackground');
+  }
+
+  onLogout() {
+    localStorage.clear();
   }
 }
