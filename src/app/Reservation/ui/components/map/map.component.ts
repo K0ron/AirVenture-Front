a7f778571f77ba0228a0d2730e84f69b161056cd
Kevin Caron <kevin.caron.91@gmail.com>
2024-08-20 19:24:42 +0200

    commit before update angular

--- src/app/Reservation/ui/components/map/map.component.ts
+++ src/app/Reservation/ui/components/map/map.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { Loader } from '@googlemaps/js-api-loader';
+import * as mapboxgl from 'mapbox-gl';
+
+interface MapboxGL {
+  accessToken: string;
+}
+
+interface FeatureProperties {
+  name: string;
+  description: string;
+  url: string;
+}
+
+const mapbox = mapboxgl as any as MapboxGL;
 
 @Component({
   selector: 'app-map',
@@ -8,37 +20,44 @@ import { Loader } from '@googlemaps/js-api-loader';
   standalone: true,
 })
 export class MapComponent implements OnInit {
-  private map!: google.maps.Map;
+  ngOnInit(): void {
+    this.setAccessToken(
+      'pk.eyJ1IjoiazByb24iLCJhIjoiY2x2NmRraWp1MGJ6OTJtb2Y5NmRybThkMyJ9.v5oyKoLPsDNr5GLc42Ja5A'
+    );
 
-  ngOnInit() {
-    const center = { lat: 41.90476224706472, lng: 12.49822074385094 };
-    const zoom = 14;
-    const url = 'https://maps.googleapis.com/maps/api/staticmap';
-
-    // @ts-ignore google.maps.plugins
-    const loader = new Loader({
-      apiKey: 'YOUR_API_KEY',
-      version: 'weekly',
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [2.3522, 48.8566], // starting position [lng, lat]
+      zoom: 12, // starting zoom
     });
 
-    document.addEventListener('DOMContentLoaded', () => {
-      const wrapper = document.getElementById('wrapper') as HTMLButtonElement;
+    map.on('load', () => {
 
-      wrapper.style.backgroundImage = `url(${url}?center=${center.lat},${center.lng}&zoom=${zoom}&scale=2&size=${wrapper.clientWidth}x${wrapper.clientHeight}&key=YOUR_API_KEY)`;
 
-      wrapper.addEventListener('click', () => {
-        wrapper.remove();
-
-        loader.load().then(() => {
-          this.map = new google.maps.Map(
-            document.getElementById('map') as HTMLElement,
+      map.addSource('points-of-interest', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [
             {
-              center,
-              zoom,
-            }
-          );
-        });
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [2.3522, 48.8566], // coordenadas de la Torre Eiffel
+              },
+              properties: {
+                name: 'Eiffel Tower',
+                description: 'Un punto de referencia icónico en París',
+                url: '/profile', // URL local de tu aplicación
+              },
+            },
+          ],
+        },
       });
     });
+  }
+  private setAccessToken(token: string) {
+    mapbox.accessToken = token;
   }
 }
